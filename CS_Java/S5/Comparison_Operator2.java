07fbaed1c920b64a9744adc8e91e5750c270c3c5
Arun Kumar <vysyarajuarun.77@gmail.com>
2021-04-11 22:36:54 +0530

    S5 - 31 Comparison Operator2

---
+++ CS_Java/S5/Comparison_Operator2.java
@@ -0,0 +1,45 @@
+class Comparison_Operator2
+{
+	public static void main(String[] args)
+	{
+		int x = 5, y = 6;
+		if(x>5)   //Greater than
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+
+		int p = 5, q = 5;
+		if (p >= q)    //Greater than equal to 
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+
+		int x1 = 4, y1 = 3;
+		if(x1 < y1)     //Less than
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+
+		int p1 = 7 , q1 = 8;
+		if( p1 <= q1)   //Less than or equal too
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+	}
+}
